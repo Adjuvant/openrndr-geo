403ec6a30ccee92a42765400c78eb7223dc61ef2
Thomas Deacon <>
2026-02-22 22:49:55 +0000

    test: convert StaggerVerification to JUnit test
    - Changed from main() function to proper JUnit test class
    - Added @Test annotations and assertions
    - Matches format of other tests in src/test/kotlin/geo/

---
+++ src/test/kotlin/geo/animation/StaggerVerificationTest.kt
@@ -0,0 +1,41 @@
+package geo.animation
+
+import geo.Feature
+import geo.Point
+import org.junit.Test
+import org.junit.Assert.*
+import org.openrndr.math.Vector2
+
+class StaggerVerificationTest {
+
+    @Test
+    fun staggerByIndexReturnsCorrectDelays() {
+        val features = listOf(
+            Feature(Point(0.0, 0.0)),
+            Feature(Point(1.0, 0.0)),
+            Feature(Point(2.0, 0.0))
+        )
+
+        val staggered = features.asSequence().staggerByIndex(50L).toList()
+
+        assertEquals("First feature should have 0ms delay", 0L, staggered[0].delay)
+        assertEquals("Second feature should have 50ms delay", 50L, staggered[1].delay)
+        assertEquals("Third feature should have 100ms delay", 100L, staggered[2].delay)
+    }
+
+    @Test
+    fun staggerByDistanceReturnsDelaysProportionalToDistance() {
+        val features = listOf(
+            Feature(Point(0.0, 0.0)),
+            Feature(Point(10.0, 0.0)),
+            Feature(Point(20.0, 0.0))
+        )
+
+        val origin = Vector2(0.0, 0.0)
+        val staggered = features.asSequence().staggerByDistance(origin, 10.0).toList()
+
+        assertEquals("First feature should have 0ms delay", 0L, staggered[0].delay)
+        assertEquals("Second feature should have 100ms delay", 100L, staggered[1].delay)
+        assertEquals("Third feature should have 200ms delay", 200L, staggered[2].delay)
+    }
+}
