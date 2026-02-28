ee697aa24d32b6649fcb83e7e5dbd75c851eaa9a
Thomas Deacon <>
2026-02-28 00:56:28 +0000

    example: add quick geo rendering example

--- examples/render/06-quick-geo.kt
+++ examples/render/06-quick-geo.kt
@@ -1,2 +1,23 @@
-package examples.render
+@file:JvmName("QuickGeo")
+package examples.anim
 
+import geo.geoSource
+import geo.render.geo
+import org.openrndr.application
+
+fun main() = application {
+    configure {
+        width = 700
+        height = 700
+    }
+
+    program {
+        // Load geographic data
+        val data = geoSource("examples/data/geo/populated_places.geojson")
+
+        extend {
+            // Draw geo data with the default style
+            drawer.geo(data)
+        }
+    }
+}
